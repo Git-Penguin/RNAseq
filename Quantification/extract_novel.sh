4afac9f73c34d7f3ccf03030d98cabd5b5b3eb29
Git-Penguin <76876392+Git-Penguin@users.noreply.github.com>
2021-01-05 02:31:39 +0100

    Update extract_novel.sh

--- Quantification/extract_novel.sh
+++ Quantification/extract_novel.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-# from metaassembly.fa used to build kallisto index
+# from metaassembly.fa used to build kallisto index extract only the genes with a StringTie identifer (not definitively mapped and thus assmumed novel)
 awk -v in_novel="FALSE" ' />/ {in_novel="FALSE"} /^>MSTRG/ {in_novel="TRUE"} {if(in_novel=="TRUE"){print $0}}' metaassembly.fa > NovelTranscripts.fa
 
