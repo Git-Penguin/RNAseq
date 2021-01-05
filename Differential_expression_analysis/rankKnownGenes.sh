635d000305d107a1fd8b45debf1442b17b3f0b1f
Git-Penguin <76876392+Git-Penguin@users.noreply.github.com>
2021-01-05 02:24:42 +0100

    Update rankKnownGenes.sh

--- Differential_expression_analysis/rankKnownGenes.sh
+++ Differential_expression_analysis/rankKnownGenes.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 #FILE HAS TO BE PRE-SORTED by qvalue for ranking
+#In reality the initial scan for these genes was done manually - during those manual checks, the original reg_expressions raised false hits because other genes contain
+#the expression as a substring - since it weren't too many i just adjusted the exclusion sets to only show the ones i wanted
+#The only reason this was retroactively put into script form is so that the results can be reproduced quickly
 KNOWNGENES=("BRCA1" "BRCA2" "ABCC2" "ERCC1" "BCL2[^LA1]" "MDM2" "EGFR[^\-]" "ERBB2" "PTEN" "AKT1[^S]" "BIRC5")
 END_OF_ARRAY=10
 GENES_TOTAL=$(wc -l < genelist_cleaned_filtered.txt)
